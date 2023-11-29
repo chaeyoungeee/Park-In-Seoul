161df63942a2eb3e64409eb18e22f6825fa9eb40
Limchaeyoung <102286483+chaeyoungeee@users.noreply.github.com>
2023-11-29 11:23:04 +0900

    디테일 액티비티 및 xml 파싱

---
+++ app/src/main/java/com/example/mpproject/DetailActivity.kt
@@ -0,0 +1,156 @@
+package com.example.mpproject
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.material.tabs.TabLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
+import com.example.mpproject.adapters.ViewPagerAdapter
+import com.example.mpproject.data.Data
+import com.example.mpproject.databinding.ActivityDetailBinding
+import com.example.mpproject.interfaces.ParkApiService
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
+import parseParkDetailData
+import retrofit2.Response
+import retrofit2.Retrofit
+import java.io.Serializable
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val name = intent.getStringExtra("name")
+        binding.parkName.text = name
+        val congestLevel = intent.getStringExtra("congestLevel")
+        binding.congestLevel.text = congestLevel
+        val img = intent.getStringExtra("img")
+        val resourceId = resources.getIdentifier(img, "drawable", packageName)
+        binding.parkImg.setImageResource(resourceId)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://openapi.seoul.go.kr:8088/")
+            .build()
+
+        val service = retrofit.create(ParkApiService::class.java)
+
+        binding.progressBar.visibility = View.VISIBLE
+
+
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val response: Response<ResponseBody> = service.getParkData(name.toString())
+
+                if (response.isSuccessful) {
+                    val responseBody: ResponseBody? = response.body()
+                    if (responseBody != null) {
+                        val data = parseParkDetailData(responseBody)
+                        Log.d("API", "결과: ${data}")
+
+                        lifecycleScope.launch(Dispatchers.Main) {
+                            binding.sunrize.text = data["sunrize"].toString()
+                            binding.sunset.text = data["sunset"].toString()
+
+                            val congestBundle = Bundle().apply {
+                                putSerializable("fcstPopulation", Data(data["fcstPopulation"]))
+                                putString("congestMsg", data["congestMsg"].toString())
+                            }
+
+                            val weatherBundle = Bundle().apply {
+                            }
+
+                            val dustBundle = Bundle().apply {
+                                putString("airMsg", data["airMsg"].toString())
+                                putString("airIdx", data["airIdx"].toString())
+                                putString("pm10", data["pm10"].toString())
+                                putString("pm10Idx", data["pm10Idx"].toString())
+                                putString("pm25", data["pm25"].toString())
+                                putString("pm25Idx", data["pm25Idx"].toString())
+                            }
+
+                            val eventBundle = Bundle().apply {
+                            }
+
+                            // Adapter에 각 Fragment에 전달할 Bundle 전달
+                            val adapter = ViewPagerAdapter(this@DetailActivity, congestBundle, weatherBundle, dustBundle, eventBundle)
+                            viewPager.adapter = adapter
+
+
+                            val tabLayoutTextArray = listOf("혼잡도", "날씨", "미세먼지", "행사")
+
+                            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                                tab.text = tabLayoutTextArray[position]
+                            }.attach()
+
+                            binding.progressBar.visibility = View.GONE
+                            binding.sunInfo.visibility = View.VISIBLE
+                            binding.viewPager.visibility = View.VISIBLE
+                        }
+
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e("API", e.toString())
+
+            } finally {
+
+// tab layout
+            }
+        }
+
+        tabLayout = binding.tabLayout
+        viewPager = binding.viewPager
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+
+        val congestBundle = Bundle().apply {
+        }
+
+        val weatherBundle = Bundle().apply {
+        }
+
+        val dustBundle = Bundle().apply {
+        }
+
+        val eventBundle = Bundle().apply {
+        }
+
+        // Adapter에 각 Fragment에 전달할 Bundle 전달
+        val adapter = ViewPagerAdapter(this@DetailActivity, congestBundle, weatherBundle, dustBundle, eventBundle)
+        viewPager.adapter = adapter
+
+
+        val tabLayoutTextArray = listOf("혼잡도", "날씨", "미세먼지", "행사")
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabLayoutTextArray[position]
+        }.attach()
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mpproject/parse/parseParkDetailData.kt
@@ -0,0 +1,271 @@
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Xml
+import com.example.mpproject.data.FCST_PPLTN
+import okhttp3.ResponseBody
+import org.xmlpull.v1.XmlPullParser
+import java.io.StringReader
+
+suspend fun parseParkDetailData(responseBody: ResponseBody): Map<Any, Any> {
+//    val xml = responseBody.string()
+
+    val parser = Xml.newPullParser()
+    parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
+//    parser.setInput(StringReader(xml))
+    parser.setInput(responseBody.charStream())
+
+    var eventType = parser.eventType
+
+    var congestMsg = ""
+    var maxTemp = ""
+    var minTemp = ""
+    var sensibleTemp = ""
+    var sunrize = ""
+    var sunset  = ""
+    var pm25Idx = ""
+    var pm25 = ""
+    var pm10Idx = ""
+    var pm10 = ""
+    var temp = ""
+    var precptMsg = ""
+    var airMsg = ""
+    var airIdx = ""
+
+
+
+
+    val fcstPopulation = mutableListOf<FCST_PPLTN>()
+
+
+    data class FCST_24(
+        var DT:String,
+        var temp: String,
+        var preciptaiton: String,
+        var preceptType: String,
+        var rainChance: String,
+        var skyStatus: String,
+    )
+
+    val fcstWeather = mutableListOf<FCST_24>()
+
+    data class EVENT_STTS(
+        var name: String,
+        var period: String,
+        var place: String
+    )
+    val event = mutableListOf<EVENT_STTS>()
+
+
+    while (eventType != XmlPullParser.END_DOCUMENT) {
+        when (eventType) {
+            XmlPullParser.START_TAG -> {
+                when (parser.name) {
+                    // 인구 혼잡 정보
+                    "AREA_CONGEST_MSG" -> {
+                        parser.next()
+                        congestMsg = parser.text
+                    }
+
+                    "FCST_PPLTN" -> {
+                        // 새로운 예보 객체를 생성
+                        var currentForecast = FCST_PPLTN(
+                            time = "",
+                            congestionLevel = "",
+                            populationMin = "",
+                            populationMax = ""
+                        )
+
+                        while (!(eventType == XmlPullParser.END_TAG && parser.name == "FCST_PPLTN")) {
+                            when (eventType) {
+                                XmlPullParser.START_TAG -> {
+                                    when (parser.name) {
+                                        "FCST_TIME" -> {
+                                            parser.next()
+                                            currentForecast.time = parser.text
+                                        }
+                                        "FCST_CONGEST_LVL" -> {
+                                            parser.next()
+                                            currentForecast.congestionLevel = parser.text
+                                        }
+                                        "FCST_PPLTN_MIN" -> {
+                                            parser.next()
+                                            currentForecast.populationMin = parser.text
+                                        }
+                                        "FCST_PPLTN_MAX" -> {
+                                            parser.next()
+                                            currentForecast.populationMax = parser.text
+                                        }
+                                    }
+                                }
+                            }
+                            eventType = parser.next()
+                        }
+
+                        // forecasts에 현재 예보를 추가
+                        fcstPopulation.add(currentForecast)
+                    }
+
+
+                    // 날씨 정보
+                    "TEMP" -> {
+                        parser.next()
+                        temp = parser.text
+                    }
+                    "PCP_MSG" -> {
+                        parser.next()
+                        precptMsg = parser.text
+                    }
+                    "MAX_TEMP" -> {
+                        parser.next()
+                        maxTemp = parser.text
+                    }
+                    "MIN_TEMP" -> {
+                        parser.next()
+                        minTemp = parser.text
+                    }
+                    "SENSIBLE_TEMP" -> {
+                        parser.next()
+                        sensibleTemp = parser.text
+                    }
+                    "SUNRISE" -> {
+                        parser.next()
+                        sunrize = parser.text
+                    }
+                    "SUNSET" -> {
+                        parser.next()
+                        sunset = parser.text
+                    }
+                    "PM25_INDEX" -> {
+                        parser.next()
+                        pm25Idx = parser.text
+                    }
+                    "PM25" -> {
+                        parser.next()
+                        pm25 = parser.text
+                    }
+                    "PM10_INDEX" -> {
+                        parser.next()
+                        pm10Idx = parser.text
+                    }
+                    "PM10" -> {
+                        parser.next()
+                        pm10 = parser.text
+                    }
+                    "AIR_IDX" -> {
+                        parser.next()
+                        airIdx = parser.text
+                    }
+                    "AIR_MSG" -> {
+                        parser.next()
+                        airMsg = parser.text
+                    }
+
+
+                    "FCST24HOURS" -> {
+                        // 새로운 예보 객체를 생성
+
+                        var currentForecast2 = FCST_24(
+                            DT =  "",
+                            temp = "",
+                            preciptaiton = "",
+                            preceptType = "",
+                            rainChance = "",
+                            skyStatus = "",
+                        )
+
+                        while (!(eventType == XmlPullParser.END_TAG && parser.name == "FCST24HOURS")) {
+                            when (eventType) {
+                                XmlPullParser.START_TAG -> {
+                                    when (parser.name) {
+                                        "FCST_DT" -> {
+                                            parser.next()
+                                            currentForecast2.DT = parser.text
+                                        }
+                                        "TEMP" -> {
+                                            parser.next()
+                                            currentForecast2.temp = parser.text
+                                        }
+                                        "PRECIPITATION" -> {
+                                            parser.next()
+                                            currentForecast2.preciptaiton = parser.text
+                                        }
+                                        "PRECPT_TYPE" -> {
+                                            parser.next()
+                                            currentForecast2.preceptType = parser.text
+                                        }
+                                        "RAIN_CHANCE" -> {
+                                            parser.next()
+                                            currentForecast2.rainChance = parser.text
+                                        }
+                                        "SKY_STTS" -> {
+                                            parser.next()
+                                            currentForecast2.skyStatus = parser.text
+                                        }
+                                    }
+                                }
+                            }
+                            eventType = parser.next()
+                        }
+
+                        fcstWeather.add(currentForecast2)
+                    }
+
+                    "EVENT_STTS" -> {
+                        var currentEvent = EVENT_STTS(
+                            name = "",
+                            period = "",
+                            place = ""
+                        )
+
+                        while (!(eventType == XmlPullParser.END_TAG && parser.name == "EVENT_STTS")) {
+                            when (eventType) {
+                                XmlPullParser.START_TAG -> {
+                                    when (parser.name) {
+                                        "EVENT_NM" -> {
+                                            parser.next()
+                                            currentEvent.name = parser.text
+                                        }
+                                        "EVENT_PERIOD" -> {
+                                            parser.next()
+                                            currentEvent.period = parser.text
+                                        }
+                                        "EVENT_PLACE" -> {
+                                            parser.next()
+                                            currentEvent.place = parser.text
+                                        }
+                                    }
+                                }
+                            }
+                            eventType = parser.next()
+                        }
+                        event.add(currentEvent)
+                    }
+
+                }
+            }
+        }
+        eventType = parser.next()
+    }
+
+
+
+    return mapOf(
+        "congestMsg" to congestMsg,
+        "maxTemp" to maxTemp,
+        "minTemp" to minTemp,
+        "sensibleTemp" to sensibleTemp,
+        "sunrize" to sunrize,
+        "sunset" to sunset,
+        "pm25Idx" to pm25Idx,
+        "pm25" to pm25,
+        "pm10Idx" to pm10Idx,
+        "pm10" to pm10,
+        "temp" to temp,
+        "precptMsg" to precptMsg,
+        "fcstPopulation" to fcstPopulation,
+        "fcstWeather" to fcstWeather,
+        "event" to event,
+        "airMsg" to airMsg,
+        "airIdx" to airIdx
+    )
+}
